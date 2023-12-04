f62a7ef9330e5ae74ace4927c3ef0ab762d8dced
maneesha.s <maneesha.s@cubettech.com>
2023-12-04 13:09:06 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,39 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .decorate("getDate", () => Date.now())
+  .state({
+    id: 1,
+    Title: "Introduction to Bun",
+  })
+  .get("/", () => "Hello Elysia!")
+  .get("/post/:id", ({ params: { id } }) => {
+    return { id };
+  })
+  .post("/post", ({ body, set, store, getDate }) => {
+    console.log(body);
+    console.log(store);
+    console.log("Created at..", getDate());
+    set.status = 201;
+    return { body };
+  })
+  .post("/context", (context) => {
+    return { context };
+  })
+  .get("/track/*", () => {
+    return "Tracking Route!";
+  })
+  .get("/tracks", () => {
+    return new Response(
+      JSON.stringify({
+        tracks: ["Dancing Feat", "Giggles"],
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
